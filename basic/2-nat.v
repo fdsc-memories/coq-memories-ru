1191853879a4ed19d98d70e9fbc16153ab553fe8
fdsc <fdsc@yandex.ru>
2020-03-15 01:38:16 +0300

    Немного поигрался с теоремами и тактиками

--- basic/2-nat.v
+++ basic/2-nat.v
@@ -36,6 +36,26 @@ rewrite -> H.
 reflexivity.
 Qed.
 
+(* rewrite -> plus_O_n перезаписывает левое условие уже доказанной теоремой
+*)
+Theorem mult_0_plus : forall n m : nat,
+  (0 + n) * m = n * m.
+Proof.
+  intros n m.
+  rewrite -> plus_O_n.
+  reflexivity.
+Qed.
+
+Theorem mult_S_1 : forall n m : nat,
+  m = S n ->
+  m * (1 + n) = m * m.
+Proof.
+	intros n m.
+	intros H.
+	rewrite -> H.
+	reflexivity.
+Qed.
+
 (* Тактика rewrite заключается в следующем
 intros H переносит гипотезу в контекст доказательства
 rewrite -> H. говорит о том, что нужно переписать в соответствии с гипотезой левую часть уравнения
@@ -64,6 +84,7 @@ rewrite <- H2.
 reflexivity.
 Qed.
 
+(* Тактику destruct смотреть ниже в plus_0_n_Vin3 *)
 
 
 (*
@@ -88,6 +109,7 @@ Fixpoint plusVin(n m : nat) : nat :=
 		end
   end.
 
+(* Функцию даже верно определить в симметричном варианте не удаётся *)
 Fixpoint plusVinA(n m : nat) : nat :=
   match n with
 	| O => 
@@ -143,6 +165,16 @@ Fixpoint plusVin2(n m : nat) : nat :=
   end.
 *)
 
+(*
+Теоремы n+0=n и 0+n=n для этой функции будет доказать проще,
+чем для более стандартной plusVin4
+Т.к. в данном случае при использовании тактики destruct n
+оба варианта для n сразу же определены
+Т.е. если есть возможность определить функцию так,
+что при разложении тактикой destruct параметра n, 
+значение функции будет определено для всех вариантов n хоть когда-нибудь для других значений,
+это уже может быть хорошо хотя бы для частных случаев
+*)
 Fixpoint plusVin3(n m : nat) : nat :=
   match n, m with
 	| O,    O    => O
@@ -151,31 +183,101 @@ Fixpoint plusVin3(n m : nat) : nat :=
 	| S n', S m' => S (S (plusVin3 n' m'))
   end.
 
-(* С этой функцией тоже не получается доказать, что 0+n = n *)
+
 Theorem plus_0_0_Vin3 : forall n : nat, (plusVin3 0 0) = 0.
 Proof.
   intros n.
   simpl. reflexivity. Qed.
 
-(* Это никак не доказывается *)
+(* С функцией plusVin3 тоже не получается доказать, что 0+n = n *)
 (*
-Theorem plus_0_n_Vin3 : forall n : nat, n > 0 -> (plusVin3 0 n) = n.
+Theorem plus_0_n_Vin3 : forall n : nat, (plusVin3 0 n) = n.
+Proof.
+  simpl.
+  reflexivity.
+Qed.
+ *)
+
+(*  --------------------------------------------------------------------------------
+Тактика  destruct
+Мы разбиваем переменную n: nat на две части, как она определена
+Грубо говоря, это множество вариантов [O | S(n': nat)]
+При этом as [| n'] говорит, 
+что в первый конструктор не передаётся никаких подстановок (т.к. он без параметров),
+а во второй конструктор S(n') передаётся подстановка n',
+то есть в дальнейшем это будет переменная n'
+
+eqn:E - это, по сути, именование гипотезы n=O или n=S(n')
+
+Далее каждый путь проходим отдельно, разделяя его оператором "-"
+(оператор называется "пуля" [bullets])
+*)
+Theorem plus_0_n_Vin3 : forall n : nat, (plusVin3 0 n) = n.
 Proof.
   intros n.
-  intros H.
+  destruct n as [| n'] eqn:E.
+  -
   simpl.
-  rewrite -> H.
-  simpl. reflexivity.
+  reflexivity.
+  -
+  simpl.
+  reflexivity.
 Qed.
+
+Theorem plus_n_0_Vin3 : forall n : nat, (plusVin3 n 0) = n.
+Proof.
+  intros n.
+  destruct n as [| n'] eqn:E.
+  -
+  simpl.
+  reflexivity.
+  -
+  simpl.
+  reflexivity.
+Qed.
+
+(* Доказательство, полностью идентичное тому, что выше,
+но записанное более кратко
+"Пули" (знаки "-") опущены, что не рекомендуется
+После доказывания одной ветки, Coq просто считает,
+что следующая команда уже относится к другой ветке
 *)
+Theorem plus_n_0_Vin3_2 : forall n : nat, (plusVin3 n 0) = n.
+Proof.
+  intros n.
+  destruct n.
+  reflexivity.
+  reflexivity.
+Qed.
 
-
-(* К сожалению, эту теорему он тоже доказать не может *)
+(* К сожалению, эту теорему он тоже доказать одним reflexivity не может *)
 (*
 Theorem plus_m_n : forall n m : nat, (plusVin3 m n) = (plusVin3 n m).
 Proof.
   intros n m.
   simpl. reflexivity. Qed.
+*)
+
+(*
+На последней ветке нужно доказать, что
+S (S (plusVin3 m' n')) = S (S (plusVin3 n' m'))
+
+Это нужно делать по индукции, но я пока не знаю как
+*)
+(*
+Theorem plus_m_n : forall n m : nat, (plusVin3 m n) = (plusVin3 n m).
+Proof.
+  intros n m.
+  destruct n as [|n'].
+-
+  destruct m as [|m'].
+  simpl. reflexivity.
+  simpl. reflexivity.
+-
+  destruct m as [|m'].
+  simpl. reflexivity.
+  simpl. reflexivity.
+Qed.
 *)
 
 Fixpoint plusVin4(n m : nat) : nat :=
@@ -184,9 +286,27 @@ Fixpoint plusVin4(n m : nat) : nat :=
 	| S n' => S(plusVin4 n' m)
   end.
 
-(* А вот это он доказывает на ура *)
+(* А вот это он доказывает на ура
+Т.е. симметричное определение plusVin3 операции сложения для него более сложное,
+чем несимметричное plusVin4
+ *)
 Theorem plus_0_n_Vin4 : forall n : nat, (plusVin4 0 n) = n.
 Proof.
   intros n.
-  simpl. reflexivity. Qed.
+  simpl. reflexivity.
+Qed.
 
+(* С другой стороны, симметричное определение plusVin3
+легко доказывает теорему ниже, а здесь возникают трудности
+*)
+(*
+Theorem plus_n_0_Vin4 : forall n : nat, (plusVin4 n 0) = n.
+Proof.
+  intros n.
+  destruct n.
+  -
+  simpl. reflexivity.
+  -
+  simpl. reflexivity.
+Qed.
+*)
